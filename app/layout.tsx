337e5f3415b7199ea37377a283bc5737a46e4795
faheem-fayaz <faheemmfayazz@gmail.com>
2025-10-18 10:06:49 +0530

    Fixed ts-ignore ESLint error in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 // ignore missing type declarations for global CSS side-effect import
-// @ts-ignore
+// @ts-expect-error
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
